efc6cea5128b18f91f95d63d50477bcc87ae3523
cayu <cayu@cayu.com.ar>
2023-05-24 03:10:44 +0000

    Subida general

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+import socket
+import html
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://flasktest:flasktest@cluster0.jbvay5m.mongodb.net/mapuche_dic?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+db = mongo.db
+
+@app.route('/palabras', methods=['GET'])
+def get_palabras():
+    palabras = db.diccMAP_20230129_V01.find()
+    return render_template('index.html',palabras=palabras)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
